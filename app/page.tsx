c875fd64555b976cade8b64966875fcd4cf1c59c
vasconcelos <di0g0vasconcelos@hotmail.com>
2024-06-26 22:01:10 -0300

    create experience footer magicButton

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import {FloatingNav} from "@/components/ui/FloatingNav";
 import {Grid} from "@/components/Grid";
 import {RecentProject} from "@/components/RecentProject";
 import {navItems} from "@/data";
+import {Clients} from "@/components/Clients";
+import {Experience} from "@/components/Experience";
 
 export default function Home() {
   return (
@@ -15,6 +17,8 @@ export default function Home() {
           <Hero />
             <Grid />
             <RecentProject />
+            <Clients />
+            <Experience />
         </div>
       </main>
   );
--- data/index.ts
+++ data/index.ts
@@ -49,8 +49,8 @@ export const gridItems = [
 
     {
         id: 5,
-        title: "Currently building a JS Animation library",
-        description: "The Inside Scoop",
+        title: "cursando engenharia da computação",
+        description: "UNIVESP",
         className: "md:col-span-3 md:row-span-2",
         imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
         titleClassName: "justify-center md:justify-start lg:justify-center",
@@ -59,7 +59,7 @@ export const gridItems = [
     },
     {
         id: 6,
-        title: "Do you want to start a project together?",
+        title: "Quer iniciar um projeto juntos?",
         description: "",
         className: "lg:col-span-2 md:col-span-3 md:row-span-1",
         imgClassName: "",
@@ -110,30 +110,45 @@ export const testimonials = [
             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
         name: "Michael Johnson",
         title: "Director of AlphaStream Technologies",
+        img: "/profile.svg",
+        width: 50,
+        height:50
     },
     {
         quote:
             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
         name: "Michael Johnson",
         title: "Director of AlphaStream Technologies",
+        img: "/profile.svg",
+        width: 50,
+        height:50
     },
     {
         quote:
             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
         name: "Michael Johnson",
         title: "Director of AlphaStream Technologies",
+        img: "/profile.svg",
+        width: 50,
+        height:50
     },
     {
         quote:
             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
         name: "Michael Johnson",
         title: "Director of AlphaStream Technologies",
+        img: "/profile.svg",
+        width: 50,
+        height:50
     },
     {
         quote:
             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
         name: "Michael Johnson",
         title: "Director of AlphaStream Technologies",
+        img: "/profile.svg",
+        width: 50,
+        height:50
     },
 ];
 
